4c385041346cacd4e853095ea13d978f7140e774
Rohit <rohitprajapati21092003@gmail.com>
2023-12-31 13:13:23 +0530

    Learned about DataTypes & Variables. User Input and Type Conversion of User Input

---
+++ 02Calculator.py
@@ -0,0 +1,8 @@
+# Simple Calculator
+a=5
+b=10
+print("Sum of a b",a+b)
+print("Diff of a b",a-b)
+print("Mul of a b",a*b)
+print("Div of a b",a/b)
+print("Mod of a b",a%b)
---
+++ 03UserInput.py
@@ -0,0 +1,15 @@
+# Input and Input type check 
+# a=input();
+# print(a);
+# print(type(a));
+
+# Input type conversion 
+# a=int(input());
+# b=int(input());
+# print(a+b);
+
+a=input();
+a=int(a);
+b=input();
+b=int(b);
+print(a+b)
